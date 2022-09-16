762ce52da0386012452aea4904b81aa10ef36988
Ivan Matejev <ivanmatejev@gmail.com>
2022-09-16 22:26:32 +0200

    Update Zadatak1 - beleske

--- src/d13_09_2022/Zadatak1.java
+++ src/d13_09_2022/Zadatak1.java
@@ -13,12 +13,14 @@ public class Zadatak1 {
 		driver.manage().window().maximize();
 		
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		Thread.sleep(2000)
-		;
+		Thread.sleep(2000);
+		
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		
 		driver.findElement(By.className("oxd-button")).click();
+		
+		Thread.sleep(5000);
 		
 		driver.findElement(By.className("oxd-input")).sendKeys("Me");
 		
